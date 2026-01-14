939bc1ff10bea5ce852e6ed20bb632c2b97691e7
Charlie <200533963@student.georgianc.on.ca>
2026-01-14 15:44:52 -0500

    Lesson 2
    Followed along with lesson 2 in class

--- 02/index.php
+++ 02/index.php
@@ -1,14 +1,42 @@
+<?php
 //1. Set Up & Start 
 
 
 //2. Code Commenting 
 
+// Inline Commenting
+
+/*
+    Multi-line Commenting
+*/
+
 
 //3. Variables, Data Tyoes, Concatenation, Conditional Statements & Echo
 
+$firstName = "Charlie"; // String
+$lastName = "Sirois-Morin"; // String
+$age = 22; // Integer
+$isStudent = true; // Boolean
+
+echo  "<p> Hello there, my name is " . $firstName . " " . $lastName . "</p>";
+
+if ($isStudent) 
+{
+    echo "<p> I am a student in this course </p>";    
+}
+else
+{
+    echo "<p>Oops I am in the wrong class</p>";
+}
 
 //4. Loosely Typed Language Demo
-
+$num1 = 1;
+$num2 = "10";
+function add(int $num1, int $num2)
+{
+    return $num1 + $num2;
+}
+echo "<p>" . add($num1, $num2) . "</p>";
 
 //5. Strict Types & Types Hints
 

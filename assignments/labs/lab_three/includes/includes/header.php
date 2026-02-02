aab83f8139bcae01c8fc97c57799e6c312b8821d
Charlie <200533963@student.georgianc.on.ca>
2026-02-01 19:31:10 -0500

    Created header.php
    Created a basic header.php file by going off the one provided in week 4

---
+++ assignments/labs/lab_three/includes/includes/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<!-- Created by guidance of header.php provided in week-4 -->
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lab 3 - Form Validation </title>
+    <link href="styles/main.css" rel="stylesheet">
+</head>
+
+<body>
+    <header>
+
+    </header>

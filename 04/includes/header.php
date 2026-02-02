d21a911dffb1e6ac15ad323546d637553a5b4c01
Charlie <200533963@student.georgianc.on.ca>
2026-02-01 21:51:12 -0500

    Added comments
    Added comments in each .file that still needed them

--- 04/includes/header.php
+++ 04/includes/header.php
@@ -10,16 +10,5 @@
 
 <body>
     <header>
-        <h1 class="site-title">
-            <img
-                src="assets/bitumi.png"
-                alt="Bake It Til You Make It Bakery"
-                class="logo">
-        </h1>
-        <nav>
-            <a href="/"> Home </a>
-            <a href="#"> About </a>
-            <a href="#"> Order Online </a>
-            <a href="#"> Contact </a>
-        </nav>
+
     </header>
